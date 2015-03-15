653a86a1e84cbeddab20dfc4d78758fb0f64b5f4
Kaylee Menard <mena0150@algonquinlive.com>
2015-03-15 14:32:52 -0400

    Restarting lightbox development

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 var $btnNav = $('.btn-nav');
 var $nav = $('.navigation');
 
-var $thumbs = $('.zodiac');
-var $lightboxImg = $('.lightbox-image');
-var $lightbox = $('.lightbox');
+var $thumbs = $('.thumbnails');
+var $lbImg = $('.lightbox-image');
+var $lb = $('.lightbox');
 var $btnClose = $('.btn-close');
 
 $btnNav.on('click', function() {
 	$nav.toggleClass('js-nav-open');
 });
 
-$thumbs.on('click', '.preview', function(e) {
-	e.preventDefault();
-	var big = $(this).attr('href');
-	$lightboxImg.attr('a', big);
-	$lightbox.attr('data-state', 'visible');
-});
+$thumbs.on('click', 'a', function(e) {
+// 	e.preventDefault();
+// 	var big = $(this).attr('.preview');
+// 	$lbImg.attr('href', big);
+// 	$lbx.attr('data-state', 'visible');
+// });
 
-$btnClose.on('click', function() {
-	$lightbox.attr('data-state', 'hidden');
+// $btnClose.on('click', function() {
+// 	$lightbox.attr('data-state', 'hidden');
 });
