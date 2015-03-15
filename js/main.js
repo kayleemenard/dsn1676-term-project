9a6a3dac50e5a5d35845961460df43545c49d0cd
Kaylee Menard <mena0150@algonquinlive.com>
2015-03-15 01:58:20 -0400

    Started writing .lightbox code

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 var $btnNav = $('.btn-nav');
 var $nav = $('.navigation');
 
+var $thumbnails = $('.zodiac');
+var $lbImg = $('.lightbox-image');
+var $lb = $('.lightbox');
+
 $btnNav.on('click', function() {
 	$nav.toggleClass('js-nav-open');
 });
+
+$thumbnails.on('click', 'a', function(e) {
+	e.preventDefault();
+	var big = $(this).attr('href');
+	$lbImg.attr('src', big);
+	$lb.attr('data-state', 'visible');
+})
