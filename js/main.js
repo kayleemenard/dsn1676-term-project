d221dc58acf054daae70ffdb9d986ce8708c85d1
Kaylee Menard <mena0150@algonquinlive.com>
2015-03-14 23:34:31 -0400

    Created classes
    added to html,
    created variables in .js
    add and define in css

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 var $btnNav = $('btn-nav');
-var $list = $('.list');
+var $nav = $('.navigation');
 
 $btnNav.on('click', function() {
-	var $li = $('<li>Constellations</li>');
-	$list.append($li);
-	$li.addClass('js-btn-nav');
-})
+	$nav.toggleClass('js-nav-open')
+});
