d9a977b0cbb9cfd2b12fc3bbb6f4de2df5f424b3
Kaylee Menard <mena0150@algonquinlive.com>
2015-04-18 00:22:22 -0400

    Making changes after validating code

--- js/main.js
+++ js/main.js
@@ -7,37 +7,26 @@ var $lbImg = $('.lightbox-image');
 var $lb = $('.lightbox');
 var $btnClose = $('.btn-close');
 
-$btnNav.on('click', function() {
-	$nav.toggleClass('js-nav-open');
-});
+$btnNav.on('click', function () { $nav.toggleClass('js-nav-open'); });
 
-$thumbs.on('click', 'a', function(e) {
+$thumbs.on('click', 'a', function (e) {
 	e.preventDefault();
 	var big = $(this).attr('href');
 	$lbImg.attr('src', big);
 	$lb.attr('data-state', 'visible');
 });
 
-$btnClose.on('click', function () {
-	$lb.attr('data-state', 'hidden');
-});
+$btnClose.on('click', function () { $lb.attr('data-state', 'hidden'); });
 
 /********************************************************** Top Animated Banner */
 var $nightSky = $('.banner');
 
 $nightSky.waypoint(function (direction) {
-	if (direction == 'up') {
-		$nightSky.addClass('js-active-sky');
-	}
-	else {
-		$nightSky.removeClass('js-active-sky');
-	}
-}, { offset: '10%' } );
+	if (direction === 'up') { $nightSky.addClass('js-active-sky'); }
+	else { $nightSky.removeClass('js-active-sky'); } }, { offset: '10%' } );
 
 /********************************************************** Star Slideshow */
-$("document").ready(function() {
-	$("#slideshow-img").cycle()
-})
+$("document").ready(function() { $("#slideshow-img").cycle() })
 
 /********************************************************** Smooth Scrolling */
 $('.nav').localScroll();
@@ -50,10 +39,5 @@ $('.site-info').localScroll();
 var $cityscape = $('.banner-bottom');
 
 $cityscape.waypoint(function (direction) {
-	if (direction == 'down') {
-		$cityscape.addClass('js-active-city');
-	}
-	else {
-		$cityscape.removeClass('js-active-city');
-	}
-}, { offset: '85%' } );
+	if (direction == 'down') { $cityscape.addClass('js-active-city'); }
+	else { $cityscape.removeClass('js-active-city'); } }, { offset: '85%' } );
